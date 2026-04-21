6ba3f4bd043924d0fc2376f77578ff36dec8bdb4
Olli Koski <olli.koski@student.careeria.fi>
2026-04-21 14:23:57 +0300

    Configure CORS for Netlify integration

--- backend/src/main/java/com/devtrack/backend/config/CorsConfig.java
+++ backend/src/main/java/com/devtrack/backend/config/CorsConfig.java
@@ -6,8 +6,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("*")
+                        .allowedOrigins("https://devtrack210426.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*");
             }
